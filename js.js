5814d44c074018261bcc87c7ceb67068f040fedc
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-07 22:20:59 -0700

    massive improvements from working with tutor

--- js.js
+++ js.js
@@ -13,6 +13,7 @@ var cityWS = $(".windSpeed");
 var cityUV= $(".uvIndex")
 var weatherIcon = $(".weatherIcon");
 var cityHistory = $(".cityHistory");
+var cityHistoryArray= "";
 
 if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
     console.log("no history")
@@ -24,15 +25,15 @@ if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
 console.log("local storage check worked");
 
 searchBtn.on("click", function(x) {
-    console.log("search button")
+    console.log("search button", cityInput.val());
     getCurrentWeather(cityInput.val());
 });
 
 
-$(document).on("click", function(y) {
-    var thisElement = $(this);
-    getCurrentWeather(thisElement.text());
-})
+// $(document).on("click", function(y) {
+//     var thisElement = $(this);
+//     getCurrentWeather(thisElement.text());
+// })
 
 getSearchHistory();
 
@@ -43,14 +44,14 @@ function getSearchHistory (){
 
 }
 console.log("hhelp")
-getCurrentWeather();
+// getCurrentWeather();
 
 function appendSearchHistory(placeholderCity){
-    var cityHistoryArray =localStorage.getItem(".cityHistory");
+    var cityHistoryArray =localStorage.getItem("searchHistory");
     for (var i = 0; i < cityHistoryArray.length; i++) {
         var newCity = $("<li>").attr("class", "newCity");
 
-        newCity.text(cityHistoryArr[i]);
+        newCity.text(cityHistoryArray[i]);
         cityHistory.prepend(placeholderCity);
     }
 }
@@ -58,7 +59,8 @@ function appendSearchHistory(placeholderCity){
 console.log("54")
 
 function getCurrentWeather (searchCity) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&APPID=" + apiKey + "&units=imperial";
+    // console.log(searchCity);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
 // I'm getting a 400 error here
     
     $.ajax({
@@ -66,26 +68,40 @@ function getCurrentWeather (searchCity) {
         method: "GET"
     })
     .then(function(weatherDetails){
+        // appendCurrentWeather (name, temp, humidity, windSpeed, UVIndex, iconName);
         var obj = {
             name: weatherDetails.name,
             temp: weatherDetails.main.temp,
             humidity: weatherDetails.main.humidity,
             windSpeed: weatherDetails.wind.speed,
-            UVIndex: weatherDetails.coord,
+            coordinates: weatherDetails.coord,
             iconName: weatherDetails.weather [0].icon
         }
+        appendCurrentWeather (obj.name, obj.temp, obj.humidity, obj.windSpeed, obj.coordinates, obj.iconName);
     })
-    appendCurrentWeather ();
+    // appendCurrentWeather (name, temp, humidity, windSpeed, UVIndex, iconName);
 }
 
-function appendCurrentWeather (name, temp, humidity, windSpeed, UVIndex, iconName) {
+function appendCurrentWeather (name, temp, humidity, windSpeed, coordinates, iconName) {
     cityName.text(name)
     todaysDate.text('($date})')
     cityTemp.text("Temperature:" + temp)
     cityHum.text ("Humidity:" + humidity + "%")
     cityWS.text(" Wind Speed:" + windSpeed + "MPH")
-    cityUV.text("UV Index:" + UVIndex)
+    // cityUV.text("UV Index:" + coordinates)
     weatherIcon.attr ("src", iconName)
+    console.log(coordinates)
+    var uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${coordinates.lat}&lon=${coordinates.lon}`;
+        
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        })
+        .then(function(weatherDetails){
+            cityUV.text("UV Index:" + weatherDetails.value)
+            console.log(weatherDetails)
+        })
+
 
 
 }
